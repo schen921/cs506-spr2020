c3400a5143ebb4f6f281399c52693ea3c434a97e
schen921 <48606083+schen921@users.noreply.github.com>
2020-02-24 16:44:16 -0600

    upload files

---
+++ FindDuplicate2.java
@@ -0,0 +1,16 @@
+class FindDuplicate2 {
+	public static int findDuplicate(int[] nums) {
+		int slow = nums[0];
+	    int fast = nums[nums[0]];
+	    while (slow != fast) {
+	        slow = nums[slow];
+	        fast = nums[nums[fast]];
+	    }
+	    fast = 0;
+	    while (slow != fast) {
+	        slow = nums[slow];
+	        fast = nums[fast];
+	    }
+	    return slow;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+	public static void main(String[] args) {
+		int[] array = new int[]{1,2,3,3,4};
+
+		int duplicate = FindDuplicate.findDuplicate(array);
+	}
+}
